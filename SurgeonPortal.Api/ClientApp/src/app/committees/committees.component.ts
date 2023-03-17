ebe5a813877eb43b6449a6f526c424b132f613b4
Joseph Mayberry <joseph.mayberry@ytg.io>
2023-03-17 14:23:21 +0000

    Merged PR 74: feature/257 - Layout the placeholder ui for the committees page
    Description of Functionality
    
    Layout the placeholder ui for the committees page
    added a placeholder (with styling) for footer text to display on each page
    updated some general styles
    
    What kind of change does this PR introduce? (Bug fix, feature, doc update, etc)
    
    feature
    
    What is the current behavior?
    
    currently there was no committees page, updated to match the placeholder from the mockups
    
    Does this PR introduce a breaking change?
    
    no
    
    Recommendations / Steps to test your PR?
    
    open the app and view the committees page (you will have to manual navigate to the page, and until login is wired up you will have to manually set the app as logged in and remove the authguard checks on the relevant page(s))
    
    List the browsers this PR was tested on.
    Chrome
    
    Related feature (copy the link to the Azure DevOps Feature below)
    #257
    #258
    
    General Checklist before submitting:
    [ ] Code style guides were reviewed and followed
    [ ] Rebased onto current target branch
    [ ] Write unit tests for your changes
    
    .NET API Checklist before submitting:
    [ ] All projects build (including unit test csproj's)
    [ ] .NET Code style guides were reviewed and followed
    [ ] Static Code Analysis rules pass on classes modified / created in this PR
    [ ] Update the PostMan collection to test your webapi changes
    
    DB Checklist before submitting:
    [ ] Database Project(s) build successfully
    [ ] DB Code style guides were reviewed and followed (links to be added)
    [ ] Reviewed Changes with API Dev to ensure completeness
    
    Web Checklist before submitting:
    [ ] The Angular app builds (including unit tests)
    [ ] Angular Code style guides were reviewed and followed
    
    Related work items: #257, #258

--- SurgeonPortal.Api/ClientApp/src/app/committees/committees.component.ts
+++ SurgeonPortal.Api/ClientApp/src/app/committees/committees.component.ts
@@ -1,9 +1,10 @@
-import { Component } from '@angular/core';
+import { Component, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 
 @Component({
   selector: 'abs-committees',
   templateUrl: './committees.component.html',
   styleUrls: ['./committees.component.scss'],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA],
   standalone: true,
 })
 export class CommitteesComponent {}
