0ff5bf99b2de3015659c49a1336d2af5396baac3
Joseph Mayberry <joseph.mayberry@ytg.io>
2023-03-27 18:33:09 +0000

    Merged PR 100: Feature185 - html and layout for top section of examination history page
    Description of Functionality
    
    html and layout for top section of examination history page
    
    What kind of change does this PR introduce? (Bug fix, feature, doc update, etc)
    
    feature
    
    What is the current behavior?
    
    page and section did not previously exist
    
    Does this PR introduce a breaking change?
    
    no
    
    Recommendations / Steps to test your PR?
    
    view relevant page in the application
    
    List the browsers this PR was tested on.
    
    Chrome
    
    Related feature (copy the link to the Azure DevOps Feature below)
    #185
    #236
    
    General Checklist before submitting:
    [ ] Code style guides were reviewed and followed
    [ ] Rebased onto current target branch
    [ ] Write unit tests for your changes
    
    .NET API Checklist before submitting:
    [ ] All projects build (including unit test csproj's)
    [ ] .NET Code style guides were reviewed and followed
    [ ] Static Code Analysis rules pass on classes modified / created in this PR
    [ ] Update the PostMan collection to test your webapi changes
    
    DB Checklist before submitting:
    [ ] Database Project(s) build successfully
    [ ] DB Code style guides were reviewed and followed (links to be added)
    [ ] Reviewed Changes with API Dev to ensure completeness
    
    Web Checklist before submitting:
    [ ] The Angular app builds (including unit tests)
    [ ] Angular Code style guides were reviewed and followed
    
    Related work items: #185, #236

--- SurgeonPortal.Api/ClientApp/src/app/examination-history/examination-history.component.ts
+++ SurgeonPortal.Api/ClientApp/src/app/examination-history/examination-history.component.ts
@@ -1,9 +1,62 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+import { Router } from '@angular/router';
+import { CollapsePanelComponent } from '../shared/components/collapse-panel/collapse-panel.component';
 
 @Component({
   selector: 'abs-examination-history',
   templateUrl: './examination-history.component.html',
   styleUrls: ['./examination-history.component.scss'],
   standalone: true,
+  schemas: [CUSTOM_ELEMENTS_SCHEMA],
+  imports: [CommonModule, CollapsePanelComponent],
 })
-export class ExaminationHistoryComponent {}
+export class ExaminationHistoryComponent {
+  // TODO: [Joe] dummy data, replace with real data
+  user = {
+    displayName: 'John Doe, M.D.',
+  };
+  // TODO: [Joe] dummy data, replace with real data
+  activeExams = [
+    {
+      id: 1,
+      examTitle: 'Metabolic Bariatric Surgery Examination',
+      startDate: new Date('3/30/22'),
+      endDate: new Date('4/2/22'),
+      status: 'Approved',
+      updates: [
+        {
+          updateDate: new Date('5/2/22'),
+          updateText: 'Application Packet Received',
+        },
+        {
+          updateDate: new Date('5/2/22'),
+          updateText: 'Application Received',
+        },
+      ],
+    },
+    {
+      id: 2,
+      examTitle: 'Metabolic Bariatric Surgery Examination',
+      startDate: new Date('3/30/22'),
+      endDate: new Date('4/2/22'),
+      status: 'Approved',
+      updates: [
+        {
+          updateDate: new Date('5/2/22'),
+          updateText: 'Application Packet Received',
+        },
+        {
+          updateDate: new Date('5/2/22'),
+          updateText: 'Application Received',
+        },
+      ],
+    },
+  ];
+
+  constructor(private _router: Router) {}
+
+  get router(): Router {
+    return this._router;
+  }
+}
