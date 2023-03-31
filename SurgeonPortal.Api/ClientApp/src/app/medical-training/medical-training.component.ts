91f801eb00f2df2a5f10c7809b6207064626be7c
Joseph Mayberry <joseph.mayberry@ytg.io>
2023-03-31 15:24:58 +0000

    Merged PR 116: feature/191 - html and layout for medical training page
    Description of Functionality
    
    html and layout for medical training page
    
    What kind of change does this PR introduce? (Bug fix, feature, doc update, etc)
    
    feature
    
    What is the current behavior?
    
    page did not previously exist
    
    Does this PR introduce a breaking change?
    
    no
    
    Recommendations / Steps to test your PR?
    
    open medical training page in the application
    
    List the browsers this PR was tested on.
    
    Chrome
    
    Related feature (copy the link to the Azure DevOps Feature below)
    #191
    #244
    
    General Checklist before submitting:
    [ ] Code style guides were reviewed and followed
    [ ] Rebased onto current target branch
    [ ] Write unit tests for your changes
    
    .NET API Checklist before submitting:
    [ ] All projects build (including unit test csproj's)
    [ ] .NET Code style guides were reviewed and followed
    [ ] Static Code Analysis rules pass on classes modified / created in this PR
    [ ] Update the PostMan collection to test your webapi changes
    
    DB Checklist before submitting:
    [ ] Database Project(s) build successfully
    [ ] DB Code style guides were reviewed and followed (links to be added)
    [ ] Reviewed Changes with API Dev to ensure completeness
    
    Web Checklist before submitting:
    [ ] The Angular app builds (including unit tests)
    [ ] Angular Code style guides were reviewed and followed
    
    Related work items: #191, #244

--- SurgeonPortal.Api/ClientApp/src/app/medical-training/medical-training.component.ts
+++ SurgeonPortal.Api/ClientApp/src/app/medical-training/medical-training.component.ts
@@ -1,9 +1,65 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { ProfileHeaderComponent } from '../shared/components/profile-header/profile-header.component';
 
 @Component({
   selector: 'abs-medical-training',
   templateUrl: './medical-training.component.html',
   styleUrls: ['./medical-training.component.scss'],
   standalone: true,
+  schemas: [CUSTOM_ELEMENTS_SCHEMA],
+  imports: [CommonModule, FormsModule, ProfileHeaderComponent],
 })
-export class MedicalTrainingComponent {}
+export class MedicalTrainingComponent {
+  isEdit = true;
+  // TODO: [Joe] faked user data, replace with real data
+  user = {
+    profilePicture:
+      'https://fastly.picsum.photos/id/91/3504/2336.jpg?hmac=tK6z7RReLgUlCuf4flDKeg57o6CUAbgklgLsGL0UowU',
+    fullName: 'John Doe',
+    title: 'M.D',
+    emailAddress: 'email@test.io',
+    status: 'Trainee',
+  };
+
+  // TODO: [Joe] faked user data, replace with real data
+  medicalTraining = {
+    statement: 'USA',
+    medicalSchool: {
+      name: 'Johns Hopkins University',
+      city: 'Baltimore',
+      state: 'MD',
+      country: 'United States',
+      degree: 'MD',
+      yearGraduated: '2010',
+    },
+    residency: {
+      programName: 'General Surgery',
+      yearOfCompletion: '2015',
+      programNotListed: 'an explanation goes here',
+    },
+    fellowship: {
+      programName: 'General Surgery',
+      yearOfCompletion: '2015',
+      programNotListed: 'an explanation goes here',
+    },
+    certifications: {
+      flsCert: null,
+      fesCert: null,
+      registeredPhysician: 'Dr. Steve Smith Jr.',
+      issueDate: new Date('10/24/2022'),
+    },
+    additionalTraining: [
+      {
+        type: 'Advanced 01 Surgery',
+        state: 'NY',
+        city: 'New York',
+        institution: 'York Hospital',
+        other: '-',
+        from: new Date('7/01/2007'),
+        to: new Date('7/01/2008'),
+      },
+    ],
+  };
+}
