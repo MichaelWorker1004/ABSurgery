a333e25f8faee830a5eca80414da1fc3a7f1cbdd
Joseph Mayberry <joseph.mayberry@ytg.io>
2023-04-20 19:20:28 +0000

    Merged PR 161: feature/208 - html and layout for exam application page
    Description of Functionality
    
    html and layout for exam application page
    
    What kind of change does this PR introduce? (Bug fix, feature, doc update, etc)
    
    feature
    
    What is the current behavior?
    
    page did not previously exist
    
    Does this PR introduce a breaking change?
    
    no
    
    Recommendations / Steps to test your PR?
    
    open application to relevant page
    
    List the browsers this PR was tested on.
    
    Chrome
    
    Related feature (copy the link to the Azure DevOps Feature below)
    #208
    #254
    
    General Checklist before submitting:
    [ ] Code style guides were reviewed and followed
    [ ] Rebased onto current target branch
    [ ] Write unit tests for your changes
    
    .NET API Checklist before submitting:
    [ ] All projects build (including unit test csproj's)
    [ ] .NET Code style guides were reviewed and followed
    [ ] Static Code Analysis rules pass on classes modified / created in this PR
    [ ] Update the PostMan collection to test your webapi changes
    
    DB Checklist before submitting:
    [ ] Database Project(s) build successfully
    [ ] DB Code style guides were reviewed and followed (links to be added)
    [ ] Reviewed Changes with API Dev to ensure completeness
    
    Web Checklist before submitting:
    [ ] The Angular app builds (including unit tests)
    [ ] Angular Code style guides were reviewed and followed
    
    Related work items: #208, #254

--- SurgeonPortal.Api/ClientApp/src/app/exam-process/exam-process.component.spec.ts
+++ SurgeonPortal.Api/ClientApp/src/app/exam-process/exam-process.component.spec.ts
@@ -1,6 +1,7 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { ExamProcessComponent } from './exam-process.component';
+import { RouterTestingModule } from '@angular/router/testing';
 
 describe('ExamProcessComponent', () => {
   let component: ExamProcessComponent;
@@ -8,7 +9,7 @@ describe('ExamProcessComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [ExamProcessComponent],
+      imports: [ExamProcessComponent, RouterTestingModule],
     }).compileComponents();
 
     fixture = TestBed.createComponent(ExamProcessComponent);
--- SurgeonPortal.Api/ClientApp/src/app/exam-process/exam-process.component.ts
+++ SurgeonPortal.Api/ClientApp/src/app/exam-process/exam-process.component.ts
@@ -1,9 +1,38 @@
-import { Component } from '@angular/core';
+import { CUSTOM_ELEMENTS_SCHEMA, Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'abs-exam-process',
   templateUrl: './exam-process.component.html',
   styleUrls: ['./exam-process.component.scss'],
   standalone: true,
+  schemas: [CUSTOM_ELEMENTS_SCHEMA],
+  imports: [CommonModule, RouterLink],
 })
-export class ExamProcessComponent {}
+export class ExamProcessComponent implements OnInit {
+  availableApplications: any[] = [];
+
+  ngOnInit(): void {
+    this.getApplications();
+  }
+
+  getApplications() {
+    this.availableApplications = [
+      {
+        name: 'Pediatric Surgery Qualifying Exam',
+        progress: 'not started',
+        continuousCertNeeded: true,
+        status: 'not-started',
+        deadline: new Date('5/10/2022'),
+      },
+      {
+        name: 'General Surgery Qualifying Exam',
+        progress: '0/10 completed',
+        continuousCertNeeded: false,
+        status: 'in-progress',
+        deadline: new Date('5/10/2022'),
+      },
+    ];
+  }
+}
