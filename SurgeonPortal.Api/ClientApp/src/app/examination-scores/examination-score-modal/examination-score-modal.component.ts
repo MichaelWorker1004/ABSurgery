4dd71faa4f890bef86f8af2fbfc1a6edf138121d
Daniel Sarria <daniel.sarria@ytg.io>
2023-08-31 20:12:18 +0000

    Merged PR 601: feature/1568 - provide ability to submit scores from exam scores roster #1568...
    Description of Functionality
    
    #1568
    
    What kind of change does this PR introduce? (Bug fix, feature, doc update, etc)
    
    feature
    
    What is the current behavior?
    
    Does this PR introduce a breaking change?
    
    Recommendations / Steps to test your PR?
    
    List the browsers this PR was tested on.
    
    Related feature (copy the link to the Azure DevOps Feature below)
    
    #1569 #1573 #1570 #1572
    
    General Checklist before submitting:
    [x] Code style guides were reviewed and followed
    [x] Rebased onto current target branch
    [ ] Write unit tests for your changes
    
    .NET API Checklist before submitting:
    [ ] All projects build (including unit test csproj's)
    [ ] .NET Code style guides were reviewed and followed
    [ ] Static Code Analysis rules pass on classes modified / created in this PR
    [ ] Update the PostMan collection to test your webapi changes
    
    DB Checklist before submitting:
    [ ] Database Project(s) build successfully
    [ ] DB Code style guides were reviewed and followed (links to be added)
    [ ] Reviewed Changes with API Dev to ensure completeness
    
    Web Checklist before submitting:
    [x] The Angular app builds (including unit tests)
    [x] Angular Code style guides were reviewed and followed
    
    Related work items: #1568, #1569, #1570, #1572, #1573

--- SurgeonPortal.Api/ClientApp/src/app/examination-scores/examination-score-modal/examination-score-modal.component.ts
+++ SurgeonPortal.Api/ClientApp/src/app/examination-scores/examination-score-modal/examination-score-modal.component.ts
@@ -2,11 +2,19 @@ import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ButtonModule } from 'primeng/button';
 import { ExaminationScoreCardComponent } from 'src/app/shared/components/examination-score-card/examination-score-card.component';
-import { isObservable } from 'rxjs';
+import { BehaviorSubject, isObservable } from 'rxjs';
 import { Store } from '@ngxs/store';
-import { UpdateCaseScore } from 'src/app/state';
+import {
+  CreateCaseScore,
+  CreateExamScore,
+  UpdateCaseScore,
+} from 'src/app/state';
 import { ICaseScoreModel } from 'src/app/api';
+import { Status } from './status.enum';
+import { IExamScoreModel } from 'src/app/api/models/ce/exam-score.model';
+import { UntilDestroy, untilDestroyed } from '@ngneat/until-destroy';
 
+@UntilDestroy()
 @Component({
   selector: 'abs-examination-score-modal',
   standalone: true,
@@ -16,28 +24,75 @@ import { ICaseScoreModel } from 'src/app/api';
 })
 export class ExaminationScoreModalComponent implements OnInit {
   @Input() candidateData: any;
+  @Input() selectedStatusOption: any;
+  @Input() examScheduleId!: any;
   @Output() closeDialog = new EventEmitter();
 
   candidateCaseScores = {} as any;
   localCandidateData = {} as any;
 
+  Status = Status;
+  examStatus!: string;
+
+  examLength!: number;
+
+  disableSubmitExamScore = true;
+
+  scoredExams = 0;
+
   constructor(private _store: Store) {}
 
   ngOnInit(): void {
+    this.localCandidateData = {};
     if (isObservable(this.candidateData)) {
-      this.candidateData.subscribe((data) => {
+      this.candidateData.pipe(untilDestroyed(this)).subscribe((data: any) => {
         this.localCandidateData = data;
+        this.examLength = data?.cases?.length;
       });
     } else {
       this.localCandidateData = this.candidateData;
     }
+
+    this.selectedStatusOption.subscribe((status: string) => {
+      this.examStatus = status;
+    });
   }
 
   handleChange(event: any) {
     this.candidateCaseScores[event.case.examCaseId] = { ...event.case };
+
+    const scores: number[] = [];
+
+    Object.entries(this.candidateCaseScores).forEach(([key, value]) => {
+      const data = value as ICaseScoreModel;
+      if (data?.score) {
+        scores.push(data?.score);
+      }
+    });
+
+    if (scores.length === this.examLength) {
+      this.disableSubmitExamScore = false;
+    }
   }
 
-  saveScores() {
+  async submitScores() {
+    await this.saveScores();
+    const model = {
+      examScheduleId: this.examScheduleId,
+    } as IExamScoreModel;
+
+    this._store
+      .dispatch(new CreateExamScore(model, false))
+      .pipe(untilDestroyed(this))
+      .subscribe(() => {
+        this.resetData();
+        this.closeDialog.emit(true);
+      });
+  }
+
+  async saveScores(closeDialog = false) {
+    const promises: Promise<any>[] = [];
+
     Object.entries(this.candidateCaseScores).forEach(([key, value]) => {
       const data = value as ICaseScoreModel;
       const model = {
@@ -49,13 +104,37 @@ export class ExaminationScoreModalComponent implements OnInit {
         criticalFail: data?.criticalFail,
         remarks: data?.remarks,
       } as ICaseScoreModel;
-      this._store.dispatch(new UpdateCaseScore(model));
+
+      if (data?.examScoringId) {
+        promises.push(
+          this._store.dispatch(new UpdateCaseScore(model)).toPromise()
+        );
+
+        this.resetData();
+        this.closeDialog.emit();
+      } else {
+        promises.push(
+          this._store.dispatch(new CreateCaseScore(model)).toPromise()
+        );
+      }
     });
-    this.closeDialog.emit();
+
+    await Promise.all(promises);
+
+    if (closeDialog) {
+      this.resetData();
+      this.closeDialog.emit();
+    }
   }
 
   cancel() {
+    this.resetData();
     this.closeDialog.emit();
+  }
+
+  resetData() {
+    this.disableSubmitExamScore = true;
+    this.candidateCaseScores = {};
     this.localCandidateData = {};
     this.candidateCaseScores = {};
   }
