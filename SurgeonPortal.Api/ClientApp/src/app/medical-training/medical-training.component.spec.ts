342307a000e3270a5ae1addd662c235b5c0d385f
Joseph Mayberry <joseph.mayberry@ytg.io>
2023-10-24 10:05:13 -0400

    feature/1867 - base unit tests for medical training page

--- SurgeonPortal.Api/ClientApp/src/app/medical-training/medical-training.component.spec.ts
+++ SurgeonPortal.Api/ClientApp/src/app/medical-training/medical-training.component.spec.ts
@@ -1,28 +1,114 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-import { NgxsModule } from '@ngxs/store';
+import { NgxsModule, Store } from '@ngxs/store';
 import { MedicalTrainingComponent } from './medical-training.component';
 import { surgeonPortalState } from '../state/surgeon-portal.state';
 import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { ReactiveFormsModule } from '@angular/forms';
+import { TranslateModule } from '@ngx-translate/core';
+import { BehaviorSubject, of } from 'rxjs';
+import { SetUnsavedChanges } from '../state';
+import { IMedicalTrainingModel } from '../api/models/medicaltraining/medical-training.model';
 
 describe('MedicalTrainingComponent', () => {
   let component: MedicalTrainingComponent;
+  let store: Store;
   let fixture: ComponentFixture<MedicalTrainingComponent>;
+
+  // let medicalTrainingSubject: BehaviorSubject<IMedicalTrainingModel> =
+  //   new BehaviorSubject<IMedicalTrainingModel>(
+  //     {} as unknown as IMedicalTrainingModel
+  //   );
+
+  const medicalTrainingData: IMedicalTrainingModel = {
+    id: 1,
+    userId: 1,
+    graduateProfileId: 1,
+    graduateProfileDescription: 'test',
+    medicalSchoolName: 'test',
+    medicalSchoolCity: 'test',
+    medicalSchoolStateId: 'test',
+    medicalSchoolStateName: 'test',
+    medicalSchoolCountryId: 'test',
+    medicalSchoolCountryName: 'test',
+    degreeId: 1,
+    degreeName: 'test',
+    medicalSchoolCompletionYear: 'test',
+    residencyProgramName: 'test',
+    residencyCompletionYear: 'test',
+    residencyProgramOther: 'test',
+  } as unknown as IMedicalTrainingModel;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [
         MedicalTrainingComponent,
+        ReactiveFormsModule,
         NgxsModule.forRoot(surgeonPortalState),
         HttpClientTestingModule,
+        TranslateModule.forRoot(),
       ],
     }).compileComponents();
 
     fixture = TestBed.createComponent(MedicalTrainingComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
+
+    store = TestBed.inject(Store);
+    spyOn(store, 'dispatch').and.returnValue(of({}));
+
+    //medicalTrainingSubject = new BehaviorSubject<IMedicalTrainingModel>(medicalTrainingData);
+    //component.medicalTraining$ = medicalTrainingSubject.asObservable();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should initialize form controls', () => {
+    expect(component.medicalTrainingForm).toBeDefined();
+    // Add more specific tests for form controls if needed
+  });
+
+  it('should set edit mode to true', () => {
+    component.toggleFormEdit(true);
+    expect(component.isEdit).toBe(true);
+    expect(store.dispatch).toHaveBeenCalledWith(new SetUnsavedChanges(true));
+  });
+
+  it('should set edit mode to false', () => {
+    component.toggleFormEdit(false);
+    expect(component.isEdit).toBe(false);
+    expect(store.dispatch).toHaveBeenCalledWith(new SetUnsavedChanges(false));
+  });
+
+  it('should launch or close the fellowship modal', () => {
+    component.showFellowshipModal(true);
+    expect(component.showFellowshipAddEdit).toBe(true);
+
+    component.showFellowshipModal(false);
+    expect(component.showFellowshipAddEdit).toBe(false);
+  });
+
+  it('should launch or close the Training modal', () => {
+    component.showTrainingModal(true);
+    expect(component.showTrainingAddEdit).toBe(true);
+
+    component.showTrainingModal(false);
+    expect(component.showTrainingAddEdit).toBe(false);
+  });
+
+  it('should launch or close the Other Certificate modal', () => {
+    component.showOtherCertificaions(true);
+    expect(component.showRPVICertificatesAddEdit).toBe(true);
+
+    component.showOtherCertificaions(false);
+    expect(component.showRPVICertificatesAddEdit).toBe(false);
+  });
+
+  // it('should set initial value of the form', () => {
+  //   //component.ngOnInit();
+  //   medicalTrainingSubject.next(medicalTrainingData);
+
+  //   expect(component.createMode).toBe(false);
+  // });
 });
--- SurgeonPortal.Api/ClientApp/src/app/shared/components/training-add-edit-modal/training-add-edit-modal.component.spec.ts
+++ SurgeonPortal.Api/ClientApp/src/app/shared/components/training-add-edit-modal/training-add-edit-modal.component.spec.ts
@@ -1,9 +1,13 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { HttpClientTestingModule } from '@angular/common/http/testing';
-import { NgxsModule } from '@ngxs/store';
+import { NgxsModule, Store } from '@ngxs/store';
 import { surgeonPortalState } from '../../../state/surgeon-portal.state';
 
 import { TrainingAddEditModalComponent } from './training-add-edit-modal.component';
+import { ReactiveFormsModule } from '@angular/forms';
+import { TranslateModule } from '@ngx-translate/core';
+import { GlobalDialogService } from '../../services/global-dialog.service';
+import { SetUnsavedChanges } from 'src/app/state';
 
 describe('TrainingAddEditModalComponent', () => {
   let component: TrainingAddEditModalComponent;
@@ -13,8 +17,10 @@ describe('TrainingAddEditModalComponent', () => {
     await TestBed.configureTestingModule({
       imports: [
         HttpClientTestingModule,
+        ReactiveFormsModule,
         TrainingAddEditModalComponent,
         NgxsModule.forRoot(surgeonPortalState),
+        TranslateModule.forRoot(),
       ],
     }).compileComponents();
 
@@ -25,5 +31,51 @@ describe('TrainingAddEditModalComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should initialize form controls and observables', () => {
+    expect(component.additionalTrainingForm).toBeTruthy();
+    expect(component.isEdit).toBeFalsy();
+    // Add more expectations as needed
+  });
+
+  it('should handle cancel without unsaved changes', () => {
+    const spy = spyOn(component.cancelDialog, 'emit');
+    component.hasUnsavedChanges = false;
+    component.cancel();
+
+    expect(spy).toHaveBeenCalled();
+  });
+
+  it('should handle cancel with unsaved changes', () => {
+    const globalDialogService = TestBed.inject(GlobalDialogService);
+    spyOn(globalDialogService, 'showConfirmation').and.returnValue(
+      Promise.resolve(true)
+    );
+
+    component.hasUnsavedChanges = true;
+    component.cancel();
+
+    expect(globalDialogService.showConfirmation).toHaveBeenCalled();
+    // figure out how to trigger a confirm click on the dialog
+    //expect(component.hasUnsavedChanges).toBe(false);
+    // Add more expectations as needed
+  });
+
+  it('should handle save', () => {
+    const spy = spyOn(component.saveDialog, 'emit');
+    const store = TestBed.inject(Store);
+    spyOn(store, 'dispatch');
+    component.isEdit = true;
+    const formValue = {
+      /* mock form values */
+    };
+
+    component.save();
+
+    expect(component.hasUnsavedChanges).toBe(false);
+    expect(store.dispatch).toHaveBeenCalledWith(new SetUnsavedChanges(false));
+    expect(spy).toHaveBeenCalled();
+    // Add more expectations as needed
   });
 });
