ac916dc068231158194f7f15be3b2be842352d53
Joseph Mayberry <joseph.mayberry@ytg.io>
2023-10-30 12:37:01 -0400

    feature/1903 - finished unit tests for scoring pages

--- SurgeonPortal.Api/ClientApp/src/app/examination-scores/examination-scores.component.spec.ts
+++ SurgeonPortal.Api/ClientApp/src/app/examination-scores/examination-scores.component.spec.ts
@@ -1,14 +1,59 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { ExaminationScoresComponent } from './examination-scores.component';
-import { NgxsModule } from '@ngxs/store';
+import { NgxsModule, Store } from '@ngxs/store';
 import { surgeonPortalState } from '../state/surgeon-portal.state';
 import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { TranslateModule } from '@ngx-translate/core';
+import { GetExamScoresList } from '../state';
 
 describe('ExaminationScoresComponent', () => {
   let component: ExaminationScoresComponent;
   let fixture: ComponentFixture<ExaminationScoresComponent>;
+  let store: Store;
+
+  const mockState = {
+    examScoring: {
+      selectedExamScores: [
+        {
+          examScoringId: 1,
+          examCaseId: 1,
+          caseNumber: '1',
+          examinerUserId: 1,
+          examineeUserId: 1,
+          examineeFirstName: 'John',
+          examineeMiddleName: '',
+          examineeLastName: 'Doe',
+          examineeSuffix: '',
+          examDate: '2021-01-01',
+          startTime: '08:00',
+          endTime: '12:00',
+          score: 1,
+          criticalFail: false,
+          remarks: '',
+          isLocked: false,
+        },
+        {
+          examScoringId: 2,
+          examCaseId: 2,
+          caseNumber: '2',
+          examinerUserId: 1,
+          examineeUserId: 1,
+          examineeFirstName: 'John',
+          examineeMiddleName: '',
+          examineeLastName: 'Doe',
+          examineeSuffix: '',
+          examDate: '2021-01-01',
+          startTime: '08:00',
+          endTime: '12:00',
+          score: 2,
+          criticalFail: false,
+          remarks: '',
+          isLocked: true,
+        },
+      ],
+    },
+  };
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
@@ -20,6 +65,10 @@ describe('ExaminationScoresComponent', () => {
       ],
     }).compileComponents();
 
+    store = TestBed.inject(Store);
+    store.reset({
+      ...store.snapshot(),
+    });
     fixture = TestBed.createComponent(ExaminationScoresComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
@@ -27,5 +76,57 @@ describe('ExaminationScoresComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should initialize component properties', () => {
+    expect(component.currentYear).toEqual(new Date().getFullYear());
+    // Add more property initialization tests here
+  });
+
+  it('should call examSelected method on ngOnInit', () => {
+    spyOn(component, 'examSelected');
+    component.ngOnInit();
+    expect(component.examSelected).toHaveBeenCalled();
+  });
+
+  it('should update lockedCases property correctly', () => {
+    component.examSelected();
+    expect(component.lockedCases).toBe(false);
+  });
+
+  it('should call GetExamScoresList when getExaminationScoresDate is called', () => {
+    const getExamScoresListSpy = spyOn(store, 'dispatch');
+    component.getExaminationScoresDate(1);
+    expect(getExamScoresListSpy).toHaveBeenCalledWith(new GetExamScoresList(1));
+  });
+
+  it('should filter examinationScoresData correctly based on filterForm', () => {
+    component.examinationScoresData = [
+      { day: 'Day 1', status: 'Complete' },
+      { day: 'Day 2', status: 'Incomplete' },
+      { day: 'Day 1', status: 'Incomplete' },
+    ];
+    component.filterForm.setValue({ day: 'Day 1', status: 'Complete' });
+    component.handleFilter();
+    expect(component.filteredExaminationScoresData$.value.length).toBe(1);
+  });
+
+  it('should open the view modal and update selectedExamScheduleId$', () => {
+    const examScheduleId = 123;
+    const event = {
+      data: { examScheduleId: examScheduleId, status: 'Complete' },
+    };
+    component.handleView(event);
+    expect(component.showViewModal).toBe(true);
+    expect(component.selectedExamScheduleId$.value).toBe(examScheduleId);
+  });
+
+  it('should close the view modal and dispatch GetExamScoresList when closeDialog is called', () => {
+    const getExamScoresListSpy = spyOn(store, 'dispatch');
+    component.closeDialog(true);
+    expect(getExamScoresListSpy).toHaveBeenCalledWith(
+      new GetExamScoresList(component.examHeaderId)
+    );
+    expect(component.showViewModal).toBe(false);
   });
 });
--- SurgeonPortal.Api/ClientApp/src/app/oral-examination/oral-examination.component.spec.ts
+++ SurgeonPortal.Api/ClientApp/src/app/oral-examination/oral-examination.component.spec.ts
@@ -1,15 +1,51 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import {
+  ComponentFixture,
+  TestBed,
+  fakeAsync,
+  tick,
+} from '@angular/core/testing';
 
 import { OralExaminationsComponent } from './oral-examination.component';
 import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { RouterTestingModule } from '@angular/router/testing';
-import { NgxsModule } from '@ngxs/store';
+import { NgxsModule, Store } from '@ngxs/store';
 import { surgeonPortalState } from '../state/surgeon-portal.state';
 import { TranslateModule } from '@ngx-translate/core';
+import {
+  ClearExamineeData,
+  CreateExamScore,
+  SetExamInProgress,
+  SkipExam,
+} from '../state';
+import { IExamScoreModel } from '../api/models/ce/exam-score.model';
 
 describe('OralExaminationsComponent', () => {
   let component: OralExaminationsComponent;
   let fixture: ComponentFixture<OralExaminationsComponent>;
+  let store: Store;
+
+  const mockState = {
+    examScoring: {
+      examinee: {
+        examScheduleId: 1,
+        fullName: 'John Doe',
+        examDate: 'Oct 16 2023 12:50PM',
+        cases: [
+          {
+            id: 1,
+            title: 'case 1',
+            score: 1,
+          },
+          {
+            id: 2,
+            title: 'case 2',
+          },
+        ],
+        examineeUserId: 1,
+        examScoringId: 1,
+      },
+    },
+  };
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
@@ -22,6 +58,11 @@ describe('OralExaminationsComponent', () => {
       ],
     }).compileComponents();
 
+    store = TestBed.inject(Store);
+    store.reset({
+      ...store.snapshot(),
+      ...mockState,
+    });
     fixture = TestBed.createComponent(OralExaminationsComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
@@ -29,5 +70,100 @@ describe('OralExaminationsComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should initialize component properties', () => {
+    expect(component.currentYear).toBe(new Date().getFullYear());
+    expect(component.activeCase).toBe(0);
+    expect(component.currentIncrement).toBe(1);
+    // Add property initialization tests here
+  });
+
+  it('should clear previously selected examinee data on init', () => {
+    const clearExamineeDataSpy = spyOn(store, 'dispatch');
+    component.ngOnInit();
+    expect(clearExamineeDataSpy).toHaveBeenCalledWith(new ClearExamineeData());
+  });
+
+  it('should handle right-click event', () => {
+    const event = {
+      preventDefault: jasmine.createSpy('preventDefault'),
+    };
+    component.onRightClick(event);
+    expect(event.preventDefault).toHaveBeenCalled();
+  });
+
+  it('should get examination data and set active case', () => {
+    store.reset({
+      ...store.snapshot(),
+      ...mockState,
+    });
+    component.getExaminationData();
+
+    expect(component.activeCase).toBe(1);
+    expect(component.casesLength).toBe(2);
+    expect(component.candidateName).toBe('John Doe');
+  });
+
+  it('should handle next case and set active case', () => {
+    // Mock activeCase and casesLength properties
+    component.activeCase = 0;
+    component.casesLength = 3;
+
+    component.handleNextCase();
+
+    // Add expectations to check if active case is updated correctly
+    expect(component.activeCase).toBe(1);
+  });
+
+  it('should handle save and submit later', async () => {
+    store.reset({
+      ...store.snapshot(),
+      ...mockState,
+    });
+    component.getExaminationData();
+
+    const submitLaterSpy = spyOn(store, 'dispatch');
+    // Mock updateScores function and related properties
+    const updateScoresSpy = spyOn(component, 'updateScores').and.returnValue(
+      Promise.resolve()
+    );
+
+    component.handleSaveAndSubmitLater();
+
+    await updateScoresSpy();
+
+    expect(updateScoresSpy).toHaveBeenCalled();
+
+    expect(submitLaterSpy).toHaveBeenCalledWith(new SetExamInProgress(false));
+    expect(submitLaterSpy).toHaveBeenCalledWith(
+      new SkipExam(component.examScheduleId, '2023-10-16T04:00:00.000Z')
+    );
+  });
+
+  it('should handle submit', async () => {
+    const submitSpy = spyOn(store, 'dispatch');
+    const updateScoresSpy = spyOn(component, 'updateScores').and.returnValue(
+      Promise.resolve()
+    );
+    component.handleSubmit();
+
+    await updateScoresSpy();
+
+    expect(updateScoresSpy).toHaveBeenCalled();
+
+    expect(submitSpy).toHaveBeenCalledWith(new SetExamInProgress(false));
+    expect(submitSpy).toHaveBeenCalledWith(
+      new CreateExamScore(
+        {
+          examScheduleId: component.examScheduleId,
+        } as unknown as IExamScoreModel,
+        false
+      )
+    );
+  });
+
+  afterEach(() => {
+    TestBed.resetTestingModule();
   });
 });
