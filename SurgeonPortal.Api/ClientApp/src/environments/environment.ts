b491431d4fbfdad74b9905367cf7e45211835023
Daniel Sarria <danielsarria@Daniels-MacBook-Pro.local>
2024-01-16 13:33:55 -0500

    env change for prod

--- SurgeonPortal.Api/ClientApp/src/environments/environment.ts
+++ SurgeonPortal.Api/ClientApp/src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
   convergePayBaseUrl: 'https://www.convergepay.com',
-  credentialSearch: 'https://absurgery.org/check-a-certification',
+  credentialSearch: 'https://absurgery.org/certification',
 };
