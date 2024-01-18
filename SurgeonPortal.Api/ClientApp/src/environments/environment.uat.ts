a0a853038fd372ce76dd6b5aa36f3a6e0dcb9fe6
Joseph Mayberry <joseph.mayberry@ytg.io>
2024-01-18 10:07:32 -0500

    bug/2325 - add new uat environment build configuration

---
+++ SurgeonPortal.Api/ClientApp/src/environments/environment.uat.ts
@@ -0,0 +1,5 @@
+export const environment = {
+  production: true,
+  convergePayBaseUrl: 'https://demo.convergepay.com',
+  credentialSearch: 'https://credentialsearch.azurewebsites.net/certification',
+};
