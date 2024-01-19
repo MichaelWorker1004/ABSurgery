ed4b259c68e7c0fdb2c3596b375d1287c149295d
Evan Frisch <Evan.Frisch@ytg.io>
2024-01-19 15:20:44 -0500

    Fixed tests

--- SurgeonPortal.DataAccess.Tests/Examinations/ExamOverviewReadOnlyDalTests.cs
+++ SurgeonPortal.DataAccess.Tests/Examinations/ExamOverviewReadOnlyDalTests.cs
@@ -16,28 +16,34 @@ namespace SurgeonPortal.DataAccess.Tests.Examinations
         public async Task GetAllAsync_ExecutesSprocCorrectly()
         {
             var expectedSprocName = "[dbo].[get_exam_overview]";
-            
-        
+            var expectedUserId = 1234;
+            var expectedParams =
+                new
+                {
+                    UserId = expectedUserId
+                };
+
             var sqlManager = new MockSqlConnectionManager();
             sqlManager.AddRecords(CreateMany<ExamOverviewReadOnlyDto>());
         
             var sut = new ExamOverviewReadOnlyDal(sqlManager);
-            await sut.GetAllAsync();
+            await sut.GetAllAsync(expectedUserId);
         
             Assert.That(sqlManager.SqlConnection.ShouldCallStoredProcedure(expectedSprocName));
-            Assert.That(sqlManager.SqlConnection.ShouldPassNoParameters());
+            Assert.That(sqlManager.SqlConnection.ShouldPassParameters(expectedParams));
         }
         
         [Test]
         public async Task GetAllAsync_YieldsCorrectResult()
         {
             var expectedDtos = CreateMany<ExamOverviewReadOnlyDto>();
-        
+            var expectedUserId = 1234;
+
             var sqlManager = new MockSqlConnectionManager();
             sqlManager.AddRecords(expectedDtos);
         
             var sut = new ExamOverviewReadOnlyDal(sqlManager);
-            var result = await sut.GetAllAsync();
+            var result = await sut.GetAllAsync(expectedUserId);
         
             expectedDtos.Should().BeEquivalentTo(
                 result,
